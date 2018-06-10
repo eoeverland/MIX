74ecf8fd1108f3808f85a07201896e65ce5e3297
Edvard Øverland <edv.overland@gmail.com>
2018-06-10 16:00:39 +0200

    fikset opp syntax

--- js/script.js
+++ js/script.js
@@ -16,23 +16,18 @@ $(function(){  // $(document).ready shorthand
 });
 
 $(document).ready(function() {
-
-    /* Every time the window is scrolled ... */
-    $(window).scroll( function(){
-
-        /* Check the location of each desired element */
-        $('#ingress').each( function(i){
-
-            var bottom_of_object = $(this).position().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
-
-            /* If the object is completely visible in the window, fade it it */
-            if( bottom_of_window > bottom_of_object ){
-
-                $(this).animate({'opacity':'1'},500);
-            }
-        });
+  /* Every time the window is scrolled ... */
+  $(window).scroll( function(){
+    /* Check the location of each desired element */
+    $('#ingress').each( function(i){
+      var bottom_of_object = $(this).position().top + $(this).outerHeight();
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+      /* If the object is completely visible in the window, fade it it */
+      if( bottom_of_window > bottom_of_object ){
+        $(this).animate({'opacity':'1'},500);
+      }
     });
+  });
 });
 
 
@@ -42,4 +37,4 @@ btn.addEventListener("click", function(){
     window.location.replace('index.html');
     console.log('click');
   });
-})
+});
