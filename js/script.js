b9952c34e8435f2a8050957cd99fb0fcf3a69c3f
emiliesamuelsen <35557535+emiliesamuelsen@users.noreply.github.com>
2018-06-10 15:08:28 +0200

    Mine endringer

--- js/script.js
+++ js/script.js
@@ -9,4 +9,37 @@ $(document).ready(function(){
       };
   });
 });
-// hei alle sammen
+
+
+$(function(){  // $(document).ready shorthand
+  $('#ingress').fadeIn('slow');
+});
+
+$(document).ready(function() {
+
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+
+        /* Check the location of each desired element */
+        $('#ingress').each( function(i){
+
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+
+                $(this).animate({'opacity':'1'},500);
+            }
+        });
+    });
+});
+
+
+var btn = $('.btn');
+btn.addEventListener("click", function(){
+  btn.click(function(){
+    window.location.replace('index.html');
+    console.log('click');
+  });
+})
